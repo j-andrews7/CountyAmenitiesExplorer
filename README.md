cda11ee646a39ddc1c97941269c7605c12f63aa0
Jared Andrews <jared.andrews07@gmail.com>
2023-06-19 15:47:47 -0500

    build site

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # CountyAmenitiesExplorer
-Code for a website to serve, explore, and visualize US county amenities and cost of living.
+Code for a website to serve, explore, and visualize US county natural amenities and cost of living.
+
+The website will take a minute or two to load on initial visit, but will be quicker on subsequent loads
+
+## Data Sources
+
+To be added.
+
+## To Build/Serve Locally
+
+```
+pip install shinylive
+
+git clone https://github.com/j-andrews7/CountyAmenitiesExplorer.git
+cd CountyAmenitiesExplorer
+
+# To re-build static site.
+shinylive export app docs
+
+# To serve locally.
+python3 -m http.server --directory docs --bind localhost 8008
+```
--- app/app.py
+++ app/app.py
@@ -14,18 +14,21 @@ df = pd.read_csv(Path(__file__).parent / "data/fips-unempl-16.csv",
                    dtype={"fips": str})
 
 app_ui = ui.page_fluid(
+    ui.tags.h1("County Ammenities Explorer"),
     output_widget("map"),
 )
 
 
 def server(input, output, session):
-    mapplot = px.choropleth(df, geojson=counties, locations='fips', color='unemp',
+    fig = px.choropleth(df, geojson=counties, locations='fips', color='unemp',
                            color_continuous_scale="Viridis",
                            range_color=(0, 12),
                            scope="usa",
                            labels={'unemp':'unemployment rate'}
                           )
+    
+    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0}, width=1400, height=800)
 
-    register_widget("map", mapplot)
+    register_widget("map", fig)
 
 app = App(app_ui, server)
